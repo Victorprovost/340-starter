dcf8d13bf904e1af16e47b688292215dd1d9eceb
Victorprovost <victorop007@gmail.com>
2026-03-16 15:39:43 +0100

    new change

--- database/database_rebuild.sql
+++ database/database_rebuild.sql
@@ -1,6 +1,4 @@
--- *********************************************
 -- Create client_type data type
--- *********************************************
 
 DROP TYPE IF EXISTS public.account_type;
 
@@ -28,7 +26,7 @@ UPDATE inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
--- Query 6
+--  Query 6
 UPDATE inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
